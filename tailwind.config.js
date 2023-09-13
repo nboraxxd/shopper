e38945f97b8032b9a155fde2d5eff2717a2f641b
bora <nhatbaoxxd@gmail.com>
2023-09-14 05:58:57 +0700

    Handle UI for homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    // disable container class trong tailwind
+    container: false,
+    // disable reset CSS trong tailwind
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
